324549d699d4e1393845f59864476d9354795491
Pramit Dash <p.dash@gridx.de>
2024-01-28 16:45:56 +0100

    fix: update data-file path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.extract_data import extract_data
 
-COMMENTARY_DATA_FILE = "../data/ind_inning4_over96_aus_vs_ind_gabba_test.json"
+COMMENTARY_DATA_FILE = "data/ind_inning4_over96_aus_vs_ind_gabba_test.json"
 
 if __name__ == "__main__":
     commentary_data = extract_data(file_path=COMMENTARY_DATA_FILE)
+
